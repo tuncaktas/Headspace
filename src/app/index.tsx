4ad27fc2b1a223f05e72c48eb2570d848711b567
tuncaktas <111659476+tuncaktas@users.noreply.github.com>
2024-12-19 17:24:13 +0300

    Style the meditation list item

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,7 +9,7 @@ export default function HomeScreen() {
       <FlatList
           data={meditations}
           className= "bg-white"
-          contentContainerClassName="gap-5 p-3"
+          contentContainerClassName="gap-8 p-3"
           renderItem={({item}) => <MeditationListItem meditation={item} />}
       />
     );
--- src/components/MeditationListItem.tsx
+++ src/components/MeditationListItem.tsx
@@ -1,14 +1,25 @@
 import {Text, View} from "react-native";
 import { Meditation } from "@/types";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 
 export function MeditationListItem({meditation}: {meditation: Meditation}) {
 
     return(
-        <View className="p-5 border border-gray-300 rounded-2xl">
-            <Text className="font-semibold text-xl" >
-                {meditation.title}
-            </Text>
+        <View className="flex-row items-center gap-5">
+            <View className="bg-green-700 p-2 rounded-full">
+                <FontAwesome name="check" size={16} color="white" />
+            </View>
+            <View className="flex-1 p-5 py-8 border border-gray-300 rounded-2xl">
+                <Text className="font-semibold text-2xl mb-2" >
+                    {meditation.title}
+                </Text>
+                <View className="flex-row items-center gap-1">
+                    <FontAwesome6 name="clock" size={16} color="#6B7280" />
+                    <Text className="text-gray-600">{meditation.duration} min</Text>
+                </View>
+            </View>
         </View>
     );
 }
